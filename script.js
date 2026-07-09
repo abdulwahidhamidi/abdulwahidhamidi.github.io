5d80a4cad412b108370024e1759eecfd730f6f21
Abdul Wahid Hamidi <hamidi.eng786@gmail.com>
2026-07-09 22:12:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 console.log("Portfolio Loaded");
+function openCertificate(image){
+
+document.getElementById("certificateModal").style.display="flex";
+
+document.getElementById("certificateImage").src=image;
+
+}
+
+function closeCertificate(){
+
+document.getElementById("certificateModal").style.display="none";
+
+}
+
+window.onclick=function(e){
+
+const modal=document.getElementById("certificateModal");
+
+if(e.target===modal){
+
+closeCertificate();
+
+}
+
+}
