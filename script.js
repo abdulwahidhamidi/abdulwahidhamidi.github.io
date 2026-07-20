78a221543b9947b0efac24191c04cd38624bae4c
Abdul Wahid Hamidi <hamidi.eng786@gmail.com>
2026-07-20 12:58:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -34,6 +34,37 @@ document.querySelectorAll('nav a').forEach(anchor => {
    Active Navigation
 =========================================== */
 
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 120;
+        const sectionHeight = section.offsetHeight;
+
+        if (window.scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if(link.getAttribute("href") === "#" + current){
+
+            link.classList.add("active");
+
+        }
+
+    });
+
+});
 
 
 /* ===========================================
