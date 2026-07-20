cf64b1ffd857c853fe09b9c8a816acafe6814d1c
Abdul Wahid Hamidi <hamidi.eng786@gmail.com>
2026-07-20 13:32:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -110,3 +110,31 @@ revealSections();
 /* ===========================================
    Back To Top
 =========================================== */
+
+const backToTop = document.getElementById("backToTop");
+
+window.addEventListener("scroll", () => {
+
+    if(window.scrollY > 300){
+
+        backToTop.style.display = "flex";
+
+    }else{
+
+        backToTop.style.display = "none";
+
+    }
+
+});
+
+backToTop.addEventListener("click", () => {
+
+    window.scrollTo({
+
+        top:0,
+
+        behavior:"smooth"
+
+    });
+
+});
